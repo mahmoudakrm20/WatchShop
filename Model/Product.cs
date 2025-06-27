ee6ad178fc67225306b30c6e05e91b3a53bad094
Mahmoud Akram <mahmoudakrm20@gmail.com>
2025-06-27 16:20:19 +0300

    Project Structure

---
+++ Model/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WatchShop.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImgURL { get; set; }
+    }
+}
---
+++ Service/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using WatchShop.Model;
+namespace WatchShop.Service
+{
+    public class ProductService
+    {
+        private List<Product> products = new()
+        {
+            new Product { Id = 1, Name = "Watch 1", Price = 1000, Description = "", ImgURL = "" },
+            new Product { Id = 2, Name = "Watch 2", Price = 2000, Description = "", ImgURL = "" }
+        };
+        public List<Product> GetAll() => products;
+        public Product? GetById(int id) => products.FirstOrDefault(p => p.Id == id);
+    }
+}
