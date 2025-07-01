495ad37cd959d96299f17c8fb4e80e85ff834518
mahmoudakrm20 <mahmoudakrm20@gmail.com>
2025-07-01 11:15:14 +0300

    Form Binding For Reviews and Data Annotation Validation

--- Model/Review.cs
+++ Model/Review.cs
@@ -1,10 +1,14 @@
-﻿namespace WatchShop.Model
+﻿using System.ComponentModel.DataAnnotations;
+namespace WatchShop.Model
 {
     public class Review
     {
-        public int ProductId { get; set; } 
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
         public string UserName { get; set; } = "";
+        [Required(ErrorMessage = "Comment is required.")]
         public string Comment { get; set; } = "";
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
         public int Rating { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
